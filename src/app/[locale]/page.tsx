7894aecb10783e7008ee20cb3daab2dcad34c2b6
Fernando Lopez <fer.ragnar19@gmail.com>
2025-07-20 15:29:29 -0300

    fix metadata

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,19 +4,19 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Tuneport",
   description:
-    "The new meeting point between musicians, artists, community with history, value and ownership. Music streaming.",
+    "Where every second of music becomes value. Platform music in web3",
   openGraph: {
-    images: ["https://app.tuneport.xyz/preview.png"],
+    images: ["https://miniapp.tuneport.xyz/preview.png"],
   },
   twitter: {
     card: "summary_large_image",
     title: "Tuneport",
     description:
-      "The new meeting point between musicians, artists, community with history, value and ownership. Music streaming.",
+      "Where every second of music becomes value. Platform music in web3",
     siteId: "1467726470533754880",
     creator: "Tuneport",
     creatorId: "1467726470533754880",
-    images: ["https://app.tuneport.xyz/preview.png"],
+    images: ["https://miniapp.tuneport.xyz/preview.png"],
   },
 };
 
